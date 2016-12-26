d4a062c364df804a86703d5f688242b3ba70e158
zuiwuchang <zuiwuchang@gmail.com>
2016-12-26 16:23:39 +0800

    fix edit show save on erase all

--- public/views/App/Edit.js
+++ public/views/App/Edit.js
@@ -238,7 +238,7 @@ var NewContext = function(initObj){
 		editor.clickToolbar(name, function() {
 			modalTextarea.Show(language["input code"],"",function(modal,val){
 				modal.Hide();
-				//val = $.trim(val);
+				val = $.trim(val);
 				if(val != ""){
 					val = "<pre class='prettyprint linenums'>" + escapeHtml(val) +"</pre>";
 					editor.insertHtml(val);
@@ -583,7 +583,7 @@ var NewContext = function(initObj){
 					});
 				},
 				UpdateStatus:function(html){
-					if(html){
+					if(html || html == "" ){
 						if(html == oldVal){
 							jqSectionSaveStatus.text(language["section ok"]);
 						}else{
